a69cda2485a2b0de43da388e3103fb83ec8a4206
rahul0707 <rahul.shawn.soni@gmail.com>
2015-06-30 19:29:31 +0530

    second

--- xyz/src/xyz/Hello.java
+++ xyz/src/xyz/Hello.java
@@ -2,7 +2,8 @@ package xyz;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello world");
+		System.out.println("hiiiii");
 	}
 
 }
