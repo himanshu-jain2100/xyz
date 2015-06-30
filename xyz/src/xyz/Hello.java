c7528b4455d973dd799e6ffa7a6ba6ca4a3c8462
himanshu-jain2100 <himanshu.jain2100@gmail.com>
2015-06-30 19:39:37 +0530

    Update Hello.java
    updated from git

--- xyz/src/xyz/Hello.java
+++ xyz/src/xyz/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	   int a=10;
+	   int b=20;
+	   int c=a+b;
 		
 		System.out.println("hiiiii");
 	}
